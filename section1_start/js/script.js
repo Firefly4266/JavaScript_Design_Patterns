f53ef480508ea51629b65e29a971430ec1c38251
Firefly4266 <gaines.johnp@gmail.com>
2018-01-14 18:09:21 -0800

    completed wrapping app in an IIFE and making everything private.

--- section1_start/js/script.js
+++ section1_start/js/script.js
@@ -1,52 +1,56 @@
-var chatModule = (function() {
-		var _leadself = 'Me: ',
-		_leadcomputer = "PC: ",
-		_aSaid = ["This is a Cyber Chat"],
-		_msgYes = "Yes, that's a great idea.",
-		_msgNo = "No, that must be a mistake.",
-		_aSassyStuff = ["Like mold on books, grow myths on history.",
-						"She moved like a poem and smiled like a sphinx.",
-						"As long as we don’t die, this is gonna be one hell of a story.",
-						"She laughed, and the desert sang.",
-						"You’ve got about as much charm as a dead slug."
-					];
+//now we remove everything out of the global namespace by wrapping it in an IIFE
 
-		function talk(msg){
-			_echo(_leadself + msg);
-		};
-		
-		function replyYesNo(){
-			var msg = Math.random()<.5 ? _msgYes : _msgNo;
-			_echo(_leadcomputer + msg);
-		};
-		
-		function saySassyStuff(){
-			var msg = _aSassyStuff[ Math.floor(Math.random() * _aSassyStuff.length)];
-			_echo(_leadcomputer + msg);
-		};
-					
-		function _echo(msg) {
-			_aSaid.push("<div>" + msg + "</div>");
-			var _aSaidLength = _aSaid.length,
-			start = Math.max(_aSaidLength - 6, 0), 
-			out = "";
-		
-			for(var i = start; i < _aSaidLength; i++){
-				out += _aSaid[i];
-			}
-			$('#talk span').text(msg);
-			$('.advert').html(out);
-		};
-		return {
-			/* reveal the methods you want used here by setting them to a variable.*/
-			talk : talk,
-			replyYesNo : replyYesNo,
-			saySassyStuff : saySassyStuff
-		};	
+(function(){
+	var chatModule = (function() {
+			var _leadself = 'Me: ',
+			_leadcomputer = "PC: ",
+			_aSaid = ["This is a Cyber Chat"],
+			_msgYes = "Yes, that's a great idea.",
+			_msgNo = "No, that must be a mistake.",
+			_aSassyStuff = ["Like mold on books, grow myths on history.",
+							"She moved like a poem and smiled like a sphinx.",
+							"As long as we don’t die, this is gonna be one hell of a story.",
+							"She laughed, and the desert sang.",
+							"You’ve got about as much charm as a dead slug."
+						];
+	
+			function talk(msg){
+				_echo(_leadself + msg);
+			};
+			
+			function replyYesNo(){
+				var msg = Math.random()<.5 ? _msgYes : _msgNo;
+				_echo(_leadcomputer + msg);
+			};
+			
+			function saySassyStuff(){
+				var msg = _aSassyStuff[ Math.floor(Math.random() * _aSassyStuff.length)];
+				_echo(_leadcomputer + msg);
+			};
+						
+			function _echo(msg) {
+				_aSaid.push("<div>" + msg + "</div>");
+				var _aSaidLength = _aSaid.length,
+				start = Math.max(_aSaidLength - 6, 0), 
+				out = "";
+			
+				for(var i = start; i < _aSaidLength; i++){
+					out += _aSaid[i];
+				}
+				$('#talk span').text(msg);
+				$('.advert').html(out);
+			};
+			return {
+				/* reveal the methods you want used here by setting them to a variable.*/
+				talk : talk,
+				replyYesNo : replyYesNo,
+				saySassyStuff : saySassyStuff
+			};	
+	})();
+	
+	$(document).ready(function(){
+		chatModule.talk("Hello World");
+		chatModule.replyYesNo();
+		chatModule.saySassyStuff()
+	});
 })();
-
-$(document).ready(function(){
-	chatModule.talk("Hello World");
-	chatModule.replyYesNo();
-	chatModule.saySassyStuff()
-});
